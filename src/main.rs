cdb5aeb3bb10c543b7bb56edc7fdc79e7ea1fd2a
Afonso Lage <lage.afonso@gmail.com>
2023-03-18 17:46:13 -0300

    Changed move system to move forward

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,11 @@ fn move_player(
     if state.pressed(Action::Move) {
         let axis_data = state.axis_pair(Action::Move).unwrap();
         let move_value: Vec2 = axis_data.into();
+        let forward = transform.forward();
+        let right = transform.right();
 
-        transform.translation.x += move_value.x * time.delta_seconds();
-        transform.translation.z += move_value.y * time.delta_seconds();
+        transform.translation += move_value.x * forward * time.delta_seconds();
+        transform.translation += move_value.y * right * time.delta_seconds();
     }
 }
 
@@ -76,37 +78,58 @@ fn setup_test_environment(
 
     const HALF_SIZE: f32 = MAP_SIZE as f32 / 2.0;
     const OBSTACLE_COUNT: u32 = MAP_SIZE / 10;
-    commands.spawn(SpatialBundle::default()).insert(Name::new(format!("Obstacles"))).with_children(|parent| {
-        for x in 0..=OBSTACLE_COUNT {
-            for z in 0..=OBSTACLE_COUNT {
-                parent.spawn(PbrBundle {
-                    transform: Transform::from_xyz((x * 10) as f32 - HALF_SIZE, 0.5, (z * 10) as f32 - HALF_SIZE),
-                    ..obstacle_model.clone()
-                }).insert(Name::new(format!("{}, {}", x, z)));
+    commands
+        .spawn(SpatialBundle::default())
+        .insert(Name::new(format!("Obstacles")))
+        .with_children(|parent| {
+            for x in 0..=OBSTACLE_COUNT {
+                for z in 0..=OBSTACLE_COUNT {
+                    parent
+                        .spawn(PbrBundle {
+                            transform: Transform::from_xyz(
+                                (x * 10) as f32 - HALF_SIZE,
+                                0.5,
+                                (z * 10) as f32 - HALF_SIZE,
+                            ),
+                            ..obstacle_model.clone()
+                        })
+                        .insert(Name::new(format!("{}, {}", x, z)));
+                }
             }
-        }
-    });
+        });
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: true,
             ..Default::default()
         },
-        transform: Transform::from_xyz(0.0, 100.0, 0.0).with_rotation(Quat::from_rotation_x(-PI / 4.0)),
+        transform: Transform::from_xyz(0.0, 100.0, 0.0)
+            .with_rotation(Quat::from_rotation_x(-PI / 4.0)),
         ..Default::default()
     });
 
     // ground plane
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane { size: MAP_SIZE as f32}.into()),
+        mesh: meshes.add(
+            shape::Plane {
+                size: MAP_SIZE as f32,
+            }
+            .into(),
+        ),
         material: materials.add(Color::SILVER.into()),
         ..default()
     });
 }
 
+#[derive(Component, Debug, Clone, Copy)]
+struct MainCamera;
+
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 6.0, 12.0).looking_at(Vec3::Y, Vec3::Y),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 6.0, 12.0).looking_at(Vec3::Y, Vec3::Y),
+            ..default()
+        },
+        MainCamera,
+    ));
 }
