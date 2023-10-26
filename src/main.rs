3f61327415f47aeb60a791f90db6080a41c01abe
Afonso Lage <lage.afonso@gmail.com>
2023-10-26 04:48:14 -0300

    Added FlyByCam

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ use bevy::{
 };
 use leafwing_input_manager::prelude::*;
 
+mod fly_by_cam;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(RenderPlugin {
@@ -22,12 +24,22 @@ fn main() {
             },
         }))
         .add_plugins(InputManagerPlugin::<Action>::default())
-        .add_systems(Update, move_player)
+        .add_plugins(fly_by_cam::FlyByCameraPlugin)
+        .add_systems(
+            Update,
+            (move_player, toggle_camera, bevy::window::close_on_esc),
+        )
+        .init_resource::<PlayerControllerConfig>()
         .add_systems(
             Startup,
             (setup_camera, setup_test_environment, spawn_player),
         )
         .run();
+}
+
+#[derive(Resource, Default)]
+struct PlayerControllerConfig {
+    active: bool,
 }
 
 #[derive(Actionlike, PartialEq, PartialOrd, Clone, Copy, Hash, Debug, Reflect)]
@@ -60,10 +72,26 @@ fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     ));
 }
 
+fn toggle_camera(
+    input: Res<Input<KeyCode>>,
+    mut player_controller_config: ResMut<PlayerControllerConfig>,
+    mut camera_config: ResMut<fly_by_cam::FlyByCameraConfig>,
+) {
+    if input.just_released(KeyCode::F1) {
+        player_controller_config.active = !player_controller_config.active;
+        camera_config.active = !camera_config.active;
+    }
+}
+
 fn move_player(
     mut query: Query<(&ActionState<Action>, &mut Transform), With<Player>>,
+    player_controller_config: Res<PlayerControllerConfig>,
     time: Res<Time>,
 ) {
+    if !player_controller_config.active {
+        return;
+    }
+
     let (state, mut transform) = query.single_mut();
 
     if state.pressed(Action::Move) {
@@ -143,6 +171,7 @@ fn setup_camera(mut commands: Commands) {
             transform: Transform::from_xyz(0.0, 6.0, 12.0).looking_at(Vec3::Y, Vec3::Y),
             ..default()
         },
+        fly_by_cam::FlyByCamera,
         MainCamera,
     ));
 }
